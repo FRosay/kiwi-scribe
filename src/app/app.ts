03e8b85e556a64a82ed0d090751421db605667e5
Scotched <felix.rosay@laposte.net>
2025-07-07 21:56:27 +0200

    feat: initialisation du footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { Navbar } from './shared/components/navbar/navbar';
+import { Footer } from '@/shared/components/footer/footer';
+import { Navbar } from '@/shared/components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
